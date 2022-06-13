68c5df0d6bbbe508729bb87582da1c85b4370047
Simion Nacu <symyon.tech@gmail.com>
2022-06-13 14:38:21 -0400

    Bumped solidity version 4->5. Made contract pausable, added multisig for  that action (pause) + test for everything new

--- contracts/ExerciseC6A.sol
+++ contracts/ExerciseC6A.sol
@@ -1,21 +1,19 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.16;
 
 contract ExerciseC6A {
-
     /********************************************************************************************/
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
-
-
+    uint constant M = 2;
     struct UserProfile {
         bool isRegistered;
         bool isAdmin;
     }
 
-    address private contractOwner;                  // Account used to deploy contract
-    mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
-
-
+    address private contractOwner; // Account used to deploy contract
+    mapping(address => UserProfile) userProfiles; // Mapping for storing user profiles
+    bool private operational = true; // Flag to indicate if contract is operational
+    address[] multiCalls = new address[](0); // Array to store multiple calls
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -24,14 +22,10 @@ contract ExerciseC6A {
     // No events
 
     /**
-    * @dev Constructor
-    *      The deploying account becomes contractOwner
-    */
-    constructor
-                                (
-                                ) 
-                                public 
-    {
+     * @dev Constructor
+     *      The deploying account becomes contractOwner
+     */
+    constructor() public {
         contractOwner = msg.sender;
     }
 
@@ -43,11 +37,15 @@ contract ExerciseC6A {
     // before a function is allowed to be executed.
 
     /**
-    * @dev Modifier that requires the "ContractOwner" account to be the function caller
-    */
-    modifier requireContractOwner()
-    {
+     * @dev Modifier that requires the "ContractOwner" account to be the function caller
+     */
+    modifier requireContractOwner() {
         require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    modifier requireOperational() {
+        require(operational, "Contract is not operational");
         _;
     }
 
@@ -55,41 +53,61 @@ contract ExerciseC6A {
     /*                                       UTILITY FUNCTIONS                                  */
     /********************************************************************************************/
 
-   /**
-    * @dev Check if a user is registered
-    *
-    * @return A bool that indicates if the user is registered
-    */   
-    function isUserRegistered
-                            (
-                                address account
-                            )
-                            external
-                            view
-                            returns(bool)
-    {
+    /**
+     * @dev Check if a user is registered
+     *
+     * @return A bool that indicates if the user is registered
+     */
+    function isUserRegistered(address account) external view returns (bool) {
         require(account != address(0), "'account' must be a valid address.");
         return userProfiles[account].isRegistered;
+    }
+
+    function isOperational() public view returns (bool) {
+        return operational;
     }
 
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
 
-    function registerUser
-                                (
-                                    address account,
-                                    bool isAdmin
-                                )
-                                external
-                                requireContractOwner
+    function registerUser(address account, bool isAdmin)
+        external
+        requireContractOwner
+        requireOperational
     {
-        require(!userProfiles[account].isRegistered, "User is already registered.");
+        require(
+            !userProfiles[account].isRegistered,
+            "User is already registered."
+        );
 
         userProfiles[account] = UserProfile({
-                                                isRegistered: true,
-                                                isAdmin: isAdmin
-                                            });
+            isRegistered: true,
+            isAdmin: isAdmin
+        });
+    }
+
+    function setContractOperational(bool mode) external {
+        require(mode != operational, "Contract is already in this mode.");
+        require(
+            userProfiles[msg.sender].isAdmin,
+            "Only admins can change operational mode."
+        );
+
+        bool isDuplicate = false;
+        for (uint i = 0; i < multiCalls.length; i++) {
+            if (multiCalls[i] == msg.sender) {
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        require(!isDuplicate, "You have already called this function.");
+
+        multiCalls.push(msg.sender);
+        if (multiCalls.length >= M) {
+            operational = mode;
+            multiCalls = new address[](0);
+        }
     }
 }
-
